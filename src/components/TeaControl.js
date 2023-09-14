e10712c18982638aa18e78fd8453ec49bb506c2c
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-09-14 13:02:51 -0700

    Add details page.

--- src/components/TeaControl.js
+++ src/components/TeaControl.js
@@ -1,26 +1,42 @@
 import React from "react";
 import TeaList from "./TeaList";
 import NewTeaForm from "./NewTeaForm";
+import TeaDetail from "./TeaDetails";
 
 class TeaControl extends React.Component {
   constructor(props){
     super(props);
     this.state= {
       mainTeaList: [],
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      selectedTea: null
     }
   }
 
   handleClick = () => {
-    this.setState(pervState => ({
-      formVisibleOnPage: !pervState.formVisibleOnPage
-    }));
+    if (this.setState.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !pervState.formVisibleOnPage
+      }));
+    }
   }
 
   handleAddingNewTeaToList = (newTea) => {
     const newMainTeaList = this.state.mainTeaList.concat(newTea);
-    this.setState({mainTeaList: newMainTeaList,
-      formVisibleOnPage: false});
+    this.setState({
+      mainTeaList: newMainTeaList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangeSelectedTea = (id) => {
+    const selectedTea = this.state.mainTeaList.filter(tea => vinyl.id === id)[0];
+    this.setState ({selectedTea: selectedTea});
   }
 
   render() {
@@ -28,10 +44,16 @@ class TeaControl extends React.Component {
     let buttonText = null;
     let button = null;
 
-    if (this.state.formVisibleOnPage) {
+    if (this.state.selectedTea != null) {
+      currentlyVisibleState = <TeaDetail 
+        tea = {this.state.selectedTea} />
+        buttonText = "Return to Tea List";
+        button = <button onClick={this.handleClick}> {buttonText} </button>  
+    } else if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewTeaForm onNewTeaOrder={this.handleAddingNewTeaToList} />
+      buttonText = "Return to Tea List"
     } else {
-      currentlyVisibleState = <TeaList teaList={this.state.mainTeaList} />
+      currentlyVisibleState = <TeaList teaList={this.state.mainTeaList} onTeaSelection = {this.handleChangeSelectedTea} />
       buttonText="Add Tea";
       button = <button onClick={this.handleClick}>{buttonText}</button>
     }
---
+++ src/components/TeaDetails.js
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export default function TeaDetail (props) {
+  const { tea } = props;
+  return(
+    <>
+      <h2>Tea Details</h2>
+      <h3>Tea Type: {tea.type}</h3>
+      <h3>Tea Make: {tea.make}</h3>
+      <h4>Description: {tea.description}</h4>
+      <h4>Quantity: <em>{tea.size}</em></h4>
+    </>
+  );
+}
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object,
+}
